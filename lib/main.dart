3674a6299fd03d6f800e96314bc83bc469abb86b
niel <93116556+nielxxx@users.noreply.github.com>
2023-10-31 12:48:11 +0800

    added

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/HomePage.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFFF5F5F3),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xFFF5F5F3),
+          foregroundColor: Color(0xFF113953),
+        )),
+      routes:{
+        "/" : (context) => HomePage(),
+      }
+    );
+  }
+}
---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/widgets/ActiveChats.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Icon(Icons.notifications),
+          ),
+        ],
+      ),
+      body: ListView(children: [
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+          child: Text(
+            "Messages", 
+            style: TextStyle(
+              color: Color(0xFF113953), 
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Padding(padding: EdgeInsets.symmetric(horizontal: 15), 
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  blurRadius: 10,
+                  spreadRadius: 2,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 300,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15), 
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                Icon(
+                  Icons.search,
+                  color:Color(0xFF113953),
+                ),
+              ],
+            ),
+          ),
+        ),
+        ActiveChats(),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color(0xFF113953), 
+        child: Icon(Icons.message),
+      ),
+    );
+  }
+}
